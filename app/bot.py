2c9e11dbaaaedadb15d24c63b4c2f97d3576e595
Asier Callejo <asiercallejo20@gmail.com>
2024-12-10 23:44:03 +0100

    fix(bot) Remove arguments

--- app/bot.py
+++ app/bot.py
@@ -8,14 +8,14 @@ from utils import get_bot_token
 async def parking(update, context):
     args = context.args
     parking_command = ["python", "main.py", "parking"]
-    parking_command.append(args[0]) if args else parking_command
+    # parking_command.append(args[0]) if args else parking_command
     subprocess.run(parking_command)
 
 
 async def ester(update, context):
     args = context.args
     ester_command = ["python", "main.py", "ester"]
-    ester_command.append(args[0]) if args else ester_command
+    # ester_command.append(args[0]) if args else ester_command
     subprocess.run(ester_command)
 
 
@@ -27,8 +27,8 @@ def main():
     token = get_bot_token()
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("Lista aparcamiento", parking, has_args=True))
-    application.add_handler(CommandHandler("Indice €STER", ester, has_args=True))
+    application.add_handler(CommandHandler("Lista aparcamiento", parking, has_args=False))
+    application.add_handler(CommandHandler("Indice €STER", ester, has_args=False))
     print("Listening...")
     application.run_polling(1.0)
 
