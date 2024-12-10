4a7c8fa8d503ba8ebd454d476cb916f084a846b3
Asier Callejo <asiercallejo20@gmail.com>
2024-12-10 23:51:55 +0100

    fix(bot) Set correct commands

--- app/bot.py
+++ app/bot.py
@@ -7,6 +7,7 @@ from utils import get_bot_token
 
 async def parking(update, context):
     args = context.args
+    await update.message.reply_text("Buscando posicion en la lista del parking")
     parking_command = ["python", "main.py", "parking"]
     # parking_command.append(args[0]) if args else parking_command
     subprocess.run(parking_command)
@@ -15,6 +16,7 @@ async def parking(update, context):
 async def ester(update, context):
     args = context.args
     ester_command = ["python", "main.py", "ester"]
+    await update.message.reply_text("Extrayendo indice €STER")
     # ester_command.append(args[0]) if args else ester_command
     subprocess.run(ester_command)
 
@@ -27,8 +29,8 @@ def main():
     token = get_bot_token()
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("Lista aparcamiento", parking, has_args=False))
-    application.add_handler(CommandHandler("Indice €STER", ester, has_args=False))
+    application.add_handler(CommandHandler("parking", parking, has_args=False))
+    application.add_handler(CommandHandler("ester", ester, has_args=False))
     print("Listening...")
     application.run_polling(1.0)
 
