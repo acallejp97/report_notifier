018fdc186e769b56c54e6d07c4fbfc6216198a8d
Asier Callejo <asiercallejo20@gmail.com>
2026-06-10 22:35:07 +0200

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,6 @@
 EUROPEAN_CENTRAL_BANK_API = "https://data.ecb.europa.eu/data-detail-api/"
 ESTER_URL = EUROPEAN_CENTRAL_BANK_API + "EST.B.EU000A2X2A25.WT"
 
-BILBAO_PARKING_URL = "https://www.bilbao.eus/cs/Satellite?cid=1273001584187&language=es&pagename=Bilbaonet%2FPage%2FBIO_ListaEsperaAparcamientos&codigoParking=17&idAparcamiento=1279123164481"
+BILBAO_PARKING_URL = ("https://www.bilbao.eus/cs/Satellite?cid=1273001584187&language=es&pagename=Bilbaonet%2FPage%2FBIO"
+                      "_ListaEsperaAparcamientos&codigoParking=17&idAparcamiento=1279123164481")
 HASH_DIR = "saves"
